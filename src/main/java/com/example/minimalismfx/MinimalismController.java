ffae855f26d3ee9e8f1a7ce1b58a3e33db1fdb23
Esra <esra.salem98@gmail.com>
2023-10-02 11:03:56 +0100

    Initial commit Esra

---
+++ src/main/java/com/example/minimalismfx/MinimalismController.java
@@ -0,0 +1,20 @@
+package com.example.minimalismfx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class MinimalismController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
