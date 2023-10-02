16bfbb48b2ffc044f59ed3d3f050fd030de5ba42
Samuel Badru <samuelbadru@outlook.com>
2023-10-02 16:41:13 +0100

    Order class and order array list created

---
+++ src/main/java/com/example/minimalismfx/Admin.java
@@ -0,0 +1,12 @@
+package com.example.minimalismfx;
+
+import java.util.ArrayList;
+
+public class Admin {
+
+    ArrayList<Order> allOrders = new ArrayList<>();
+
+
+
+
+}
---
+++ src/main/java/com/example/minimalismfx/Order.java
@@ -0,0 +1,11 @@
+package com.example.minimalismfx;
+
+import java.time.LocalDateTime;
+
+public class Order {
+
+        public Order(String userName, Item item, int quantity, LocalDateTime orderDateTime) {
+
+        }
+
+}
