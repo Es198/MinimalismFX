8646f6db934df9a5a71ff9bba53cb8d6537a8148
Esra <esra.salem98@gmail.com>
2023-10-03 09:47:35 +0100

    UserClass Initial Commit V2

--- src/main/java/com/example/minimalismfx/User.java
+++ src/main/java/com/example/minimalismfx/User.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class User {
@@ -62,27 +61,29 @@ public class User {
         UserGender = userGender;
     }
 
-    public static void createUserFromCSV(){
+    public static void createUserFromCSV() {
         FileInputStream fis;
         Scanner fileScanner;
-        ArrayList<User> userList=new ArrayList<>();
-        try{
-            fis=new FileInputStream("src/main/resources/userFile.csv");
-            fileScanner=new Scanner(fis);
-            while(fileScanner.hasNextLine()){
-                String line=fileScanner.nextLine();
-                String[] userRow=line.split(",");
-                if (userRow.length==5){
-                    String userName=userRow[0].trim();
-                    String userPassword=userRow[1].trim();
-                    String userType= userRow[2].trim();
-                    int userAge=Integer.parseInt(userRow[3].trim());
-                    String userGender=userRow[4].trim();
-                    userList.add(new User(userName,userPassword,userType,userAge,userGender));
+        ArrayList<User> userList = new ArrayList<>();
+        try {
+            fis = new FileInputStream("src/main/resources/userFile.csv");
+            fileScanner = new Scanner(fis);
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                String[] userRow = line.split(",");
+                if (userRow.length == 5) {
+                    String userName = userRow[0].trim();
+                    String userPassword = userRow[1].trim();
+                    String userType = userRow[2].trim();
+                    int userAge = Integer.parseInt(userRow[3].trim());
+                    String userGender = userRow[4].trim();
+                    userList.add(new User(userName, userPassword, userType, userAge, userGender));
                 }
-            }fis.close();
+            }
+            fis.close();
         } catch (FileNotFoundException e) {
-            System.out.println("User not found");;
+            System.out.println("User not found");
+            ;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
