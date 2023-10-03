c3bf7482b92a22d15522aeec4a37568cdda9033e
Esra <esra.salem98@gmail.com>
2023-10-03 09:47:13 +0100

    UserClass Initial Commit V2

--- src/main/java/com/example/minimalismfx/User.java
+++ src/main/java/com/example/minimalismfx/User.java
@@ -3,6 +3,8 @@ package com.example.minimalismfx;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class User {
@@ -63,13 +65,21 @@ public class User {
     public static void createUserFromCSV(){
         FileInputStream fis;
         Scanner fileScanner;
-//todo: read the CSV file + need to parse it.
+        ArrayList<User> userList=new ArrayList<>();
         try{
             fis=new FileInputStream("src/main/resources/userFile.csv");
             fileScanner=new Scanner(fis);
-            fileScanner.useDelimiter(",");
             while(fileScanner.hasNextLine()){
-                System.out.println(fileScanner.nextLine());
+                String line=fileScanner.nextLine();
+                String[] userRow=line.split(",");
+                if (userRow.length==5){
+                    String userName=userRow[0].trim();
+                    String userPassword=userRow[1].trim();
+                    String userType= userRow[2].trim();
+                    int userAge=Integer.parseInt(userRow[3].trim());
+                    String userGender=userRow[4].trim();
+                    userList.add(new User(userName,userPassword,userType,userAge,userGender));
+                }
             }fis.close();
         } catch (FileNotFoundException e) {
             System.out.println("User not found");;
