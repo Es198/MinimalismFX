3f5a0980c32b8a43e3bf01c8f9fa283ae86b0638
Samuel Badru <samuelbadru@outlook.com>
2023-10-03 09:23:26 +0100

    recordOrder method implemented

--- src/main/java/com/example/minimalismfx/Admin.java
+++ src/main/java/com/example/minimalismfx/Admin.java
@@ -1,10 +1,22 @@
 package com.example.minimalismfx;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Admin {
 
     ArrayList<Order> allOrders = new ArrayList<>();
+
+    // To be called when items are purchased
+    void recordOrder(String userName, Item item, int quantity) {
+        Order order = new Order(userName, item, quantity, LocalDateTime.now());
+        allOrders.add(order);
+    }
+
+
+    String displayAllOrders(){
+        return "";
+    }
 
 
 
--- src/main/java/com/example/minimalismfx/Order.java
+++ src/main/java/com/example/minimalismfx/Order.java
@@ -3,9 +3,20 @@ package com.example.minimalismfx;
 import java.time.LocalDateTime;
 
 public class Order {
+        String userName;
+        Item item;
+        int quantity;
+        LocalDateTime orderDateTime;
 
         public Order(String userName, Item item, int quantity, LocalDateTime orderDateTime) {
-
+                this.userName = userName;
+                this.item = item;
+                this.quantity = quantity;
+                this.orderDateTime = orderDateTime;
         }
+
+
+
+
 
 }
