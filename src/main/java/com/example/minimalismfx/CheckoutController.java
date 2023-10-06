dd4791fdfde83056841281f9cab2a84d28625644
Esra <esra.salem98@gmail.com>
2023-10-06 09:31:42 +0100

    Design features V3

--- src/main/java/com/example/minimalismfx/CheckoutController.java
+++ src/main/java/com/example/minimalismfx/CheckoutController.java
@@ -1,10 +1,18 @@
 package com.example.minimalismfx;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class CheckoutController {
     @FXML
@@ -45,6 +53,29 @@ public class CheckoutController {
     public void initialize(URL url,ResourceBundle rb){
         Image image=new Image(getClass().getResourceAsStream("src/main/resources/images/moving van.jpg"));
         movingVanImage.setImage(image);
+
+    }
+
+    @FXML
+    void setUpBackToHome(ActionEvent event) throws IOException {
+        FXMLLoader loader=new FXMLLoader();
+        loader.setLocation(CheckoutController.class.getResource("minimalismFX.fxml"));
+        Parent rootParent =loader.load();
+        Scene rootScene = new Scene(rootParent);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(rootScene);
+        stage.show();
+
+    }
+    @FXML
+    void setUpLogout(ActionEvent event) throws IOException {
+        FXMLLoader loader=new FXMLLoader();
+        loader.setLocation(CheckoutController.class.getResource("LoginPageV2.fxml"));
+        Parent rootParent =loader.load();
+        Scene rootScene = new Scene(rootParent);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(rootScene);
+        stage.show();
 
     }
 }
